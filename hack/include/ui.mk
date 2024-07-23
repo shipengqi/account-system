5403aa5f09babd1b8cab28a35b2101bf32bae282
shipengqi <pooky.shipengqi@gmail.com>
2024-07-23 21:12:58 +0800

    feat: add ci workflows and other configurations

---
+++ hack/include/ui.mk
@@ -0,0 +1,8 @@
+.PHONY: ui.build
+ui.build:
+	@echo "===========> Building UI bundles"
+	@cd $(REPO_ROOT)/as-web; \
+	npm install --force && \
+	npm run build && \
+	rm -rf $(REPO_ROOT)/as-api/resources/dist && \
+	mv $(REPO_ROOT)/as-web/dist $(REPO_ROOT)/stat-api/resources
