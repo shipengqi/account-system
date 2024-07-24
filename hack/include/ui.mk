bdde65a6d104bcaae0a10d57d389688f4151fa62
shipengqi <pooky.shipengqi@gmail.com>
2024-07-24 13:15:21 +0800

    fix: ui.build error

--- hack/include/ui.mk
+++ hack/include/ui.mk
@@ -1,8 +1,12 @@
 .PHONY: ui.build
 ui.build:
 	@echo "===========> Building UI bundles"
-	@cd $(REPO_ROOT)/as-web; \
-	npm install --force && \
-	npm run build && \
-	rm -rf $(REPO_ROOT)/as-api/resources/dist && \
+	@cd $(REPO_ROOT)/as-web
+	@echo "===========> Installing Dependencies"
+	npm install --force
+	@echo "===========> Building Website"
+	npm run build
+	@echo "===========> Cleaning UI bundles"
+	rm -rf $(REPO_ROOT)/as-api/resources/dist
+	@echo "===========> Moving UI bundles"
 	mv $(REPO_ROOT)/as-web/dist $(REPO_ROOT)/as-api/resources
