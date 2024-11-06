daf6071683573fd12a932500592f3e7b82ecd8bc
shipengqi <pooky.shipengqi@gmail.com>
2024-11-06 10:29:33 +0800

    fix: lint issue

--- as-api/resources/html.go
+++ as-api/resources/html.go
@@ -1,10 +1,9 @@
-//nolint:all
 package resources
 
 import "embed"
 
 //go:embed dist/as-web/browser/index.html
-var Html []byte
+var Html []byte //nolint:all
 
 //go:embed dist/as-web/browser
-var Static embed.FS
+var Static embed.FS //nolint:all
